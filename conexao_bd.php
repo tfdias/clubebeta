18a5b6be6f3c8ace1c2cd7430b7fe85784af3137
Thiago Dias <tfdias@gmail.com>
2013-10-03 17:39:17 -0300

    Commit inicial

---
+++ conexao_bd.php
@@ -0,0 +1,15 @@
+<?php
+
+//Conexão ao Banco de Dados
+
+$conect = mysql_connect("localhost", "clubedam_user", "clubcampeao2013");
+if (!$conect) die ("<h1>Falha na coneco com o Banco de Dados!</h1>");
+$cdm_db = mysql_select_db("clubedam_newsletter");
+
+//Força query com acentuação padrão UTF-8
+mysql_query("SET NAMES 'utf8'");
+mysql_query('SET character_set_connection=utf8');
+mysql_query('SET character_set_client=utf8');
+mysql_query('SET character_set_results=utf8');
+
+?>
